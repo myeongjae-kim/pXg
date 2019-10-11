143876c35a45e4478e1142c85d2c86fd1d860ec6
Seunghyuk <progistar@naver.com>
2019-10-11 18:17:44 +0900

    init

---
+++ src/progistar/moddecoder/data/Gene.java
@@ -0,0 +1,11 @@
+package progistar.moddecoder.data;
+
+import java.util.ArrayList;
+
+public class Gene {
+
+	public String geneID = null;
+	public boolean strand = true;
+	public ArrayList<GenomicRegion> regions = new ArrayList<GenomicRegion>();
+	
+}
